3c1594fe7f489a5433036f7c0dd212adb944689b
rem-renovations <kingazrael002@gmail.com>
2025-11-05 04:09:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Toggle the mobile menu
-const menu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-menu.addEventListener('click', () => {
-  menu.classList.toggle('is-active');
-  navLinks.classList.toggle('active');
-});
